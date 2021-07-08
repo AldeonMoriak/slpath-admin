9d2a26edf0e2ab92358076f461425942438cfb36
lubuntuAldeon <mehranmirshekaran@protonmail.com>
2021-07-08 22:14:15 +0430

    change baseUrl

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
-    baseURL: 'http://localhost:4000/',
+    baseURL: 'https://slpath-backedn.herokuapp.com/',
   },
 
   router: {
